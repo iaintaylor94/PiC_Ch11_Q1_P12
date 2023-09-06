eddbf91f63784eed37d29dda10dffb61f7a79944
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-06 12:38:06 +0000

    copied program

--- .ccls-cache/@home@runner@Ch11Q1P12-summing-elements-of-an-array-v2/main.c
+++ .ccls-cache/@home@runner@Ch11Q1P12-summing-elements-of-an-array-v2/main.c
@@ -1,6 +1,26 @@
+// Function to sum the elements of an integer array Ver. 2
+
 #include <stdio.h>
 
+// Declare Functions
+int arraySum (int *, const int);
+
 int main(void) {
-  printf("Hello World\n");
+
+  int values[10] = { 3, 7, -9, 3, 6, -1, 7, 9, 1, -5 };
+
+  printf ("The sum is %d\n", arraySum (values, 10));
+  
   return 0;
+}
+
+// Define Functions
+int arraySum (int *array, const int n) {
+  int sum = 0;
+  int * const arrayEnd = array + n;
+
+  for ( ; array < arrayEnd; array++)
+    sum += *array;
+
+  return sum;
 }
